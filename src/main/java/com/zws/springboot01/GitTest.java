5874da8ccafe3740cfb8aa27a092cf34af4e427c
Shan <2710301855@qq.com>
2021-11-16 19:06:54 +0800

    first commit

---
+++ src/main/java/com/zws/springboot01/GitTest.java
@@ -0,0 +1,13 @@
+package com.zws.springboot01;
+
+/**
+ * @version 1.0
+ * @Author Shan
+ * @Date 2021/11/16 19:05
+ * @注释
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
