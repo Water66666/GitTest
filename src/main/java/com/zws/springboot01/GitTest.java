ca03a983bedda1cfbb36bfc98728ab97b6df1699
Shan <2710301855@qq.com>
2021-11-16 22:10:18 +0800

    first commit

--- src/main/java/com/zws/springboot01/GitTest.java
+++ src/main/java/com/zws/springboot01/GitTest.java
@@ -12,5 +12,7 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello hot-fix");
         System.out.println("hello master");
+        System.out.println("second master");
+        System.out.println("gitee master");
     }
 }
